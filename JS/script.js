f1d19131cc984fdd3321dd48ed72bf60a8246fc5
Artem <artemproris17@gmail.com>
2024-02-02 18:11:21 +0300

    Перенос в сприпта в отдельный файл . Проблема была в том,что скрипт запускался до подгрузки стриницы ( "DOMContentLoaded")

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+//modal
+document.addEventListener("DOMContentLoaded", function(){
+    const but = document.querySelector(".button-my-photo");
+    const modal = document.querySelector(".modal-continer");
+    const modalBack = document.querySelector(".modal-background");
+    const modalPhotoCont = document.querySelector(".modal-photos-cont");
+    const x = document.querySelector(".X");
+    const lButton = document.querySelector(".left-button");
+    const rButton = document.querySelector(".right-button");
+
+    but.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalBack.style.display = "block";
+    });
+    x.addEventListener("click", () => {
+        modal.style.display = "none";
+        modalBack.style.display = "none";
+    });
+    let countPhotoContChildren = modalPhotoCont.children.length;
+    let i = 0;
+    lButton.addEventListener("click", () => {
+        if ((i - 1) == -1) {
+            modalPhotoCont.children[i].style.display = "none";
+            i = countPhotoContChildren - 1;
+            modalPhotoCont.children[i].style.display = "block";
+        }
+        else {
+            modalPhotoCont.children[i].style.display = "none";
+            i--;
+            modalPhotoCont.children[i].style.display = "block";
+        }
+
+    });
+
+    rButton.addEventListener("click", () => {
+        if ((i + 1) == countPhotoContChildren) {
+            modalPhotoCont.children[i].style.display = "none";
+            i = 0;
+            modalPhotoCont.children[i].style.display = "block";
+        }
+        else {
+            modalPhotoCont.children[i].style.display = "none";
+            i++;
+            modalPhotoCont.children[i].style.display = "block";
+        }
+
+    });
+});
