6660b48dc03375262b0a269706efa5f6fe21982f
Artem <artemproris17@gmail.com>
2024-02-02 22:44:30 +0300

    add left menu whith social services

---
+++ JS/modalSocialScript.js
@@ -0,0 +1,18 @@
+const leftPanel = document.querySelector(".left-panel");
+const logo = document.querySelector("#logo");
+
+let f = 0 ;
+
+logo.addEventListener("click",() => {
+    if (f == 0 ){
+        leftPanel.style.display = "Block";
+        leftPanel.style.animation ="toGo 1s ease-in-out forwards";
+        f = 1;
+    }
+    else{
+        leftPanel.style.animation ="toBack 1s ease-in-out forwards";
+        f = 0;  
+    };
+    console.log(f);
+});
+
